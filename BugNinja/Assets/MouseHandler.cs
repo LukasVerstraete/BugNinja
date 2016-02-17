0b46c61e3bad28536b89de3041aed8a37d06f610
BuildTools <unconfigured@null.spigotmc.org>
2016-02-17 14:00:43 +0100

    added mouseMovementScripts

---
+++ BugNinja/Assets/MouseHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RawMouseDriver;
+using RawInputSharp;
+
+public class MouseHandler : MonoBehaviour {
+
+    private RawMouse[] mice;
+    private Vector2[] positions;
+    private RawMouseDriver.RawMouseDriver mouseDriver;
+    private const int NUM_MICE = 2;
+    private int miceAlreadyAssigned = 0;
+
+    void Start()
+    {
+        mouseDriver = new RawMouseDriver.RawMouseDriver();
+        mice = new RawMouse[NUM_MICE];
+        positions = new Vector2[NUM_MICE];
+    }
+
+    void Update()
+    {
+        // Loop through all the connected mice
+        for (int i = 0; i < mice.Length; i++)
+        {
+            try
+            {
+                mouseDriver.GetMouse(i, ref mice[i]);
+                // Cumulative movement
+                positions[i] += new Vector2(mice[i].XDelta, -mice[i].YDelta);
+            }
+            catch { }
+        }
+    }
+
+    public int GetMouse()
+    {
+        if (miceAlreadyAssigned != NUM_MICE)
+        {
+
+        }
+        return -1;
+    }
+
+    public Vector2 GetMousePosition(int mouse)
+    {
+        if(mouse > 0 && mouse <= NUM_MICE)
+            return positions[mouse];
+        return new Vector2(0,0);
+    }
+}
--- BugNinja/Assets/testScript.cs
+++ BugNinja/Assets/testScript.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RawMouseDriver;
+using RawInputSharp;
 
-public class testScript : MonoBehaviour {
+public class MouseInput : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    RawMouseDriver.RawMouseDriver mousedriver;
+    private RawMouse[] mice;
+    private Vector2[] move;
+    private const int NUM_MICE = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        mousedriver = new RawMouseDriver.RawMouseDriver();
+        mice = new RawMouse[NUM_MICE];
+        move = new Vector2[NUM_MICE];
+    }
+
+    void Update()
+    {
+        // Loop through all the connected mice
+        for (int i = 0; i < mice.Length; i++)
+        {
+            try
+            {
+                mousedriver.GetMouse(i, ref mice[i]);
+                // Cumulative movement
+                move[i] += new Vector2(mice[i].XDelta, -mice[i].YDelta);
+            }
+            catch { }
+        }
+    }
+
+    //void OnGUI()
+    //{
+    //    GUILayout.Label("Connected:");
+    //    for (int i = 0; i < mice.Length; i++)
+    //    {
+    //        if (mice[i] != null)
+    //            GUILayout.Label("[" + Mousei.ToString() + "] : " + move[i] + mice[i].Buttons[0] + mice[i].Buttons[1]);
+    //    }
+    //}
+
+    void OnApplicationQuit()
+    {
+        // Clean up
+        mousedriver.Dispose();
+    }
 }
